4c87b37ba2ff2c2dadb7b047358e312d7166067b
MarianaBatistussiGil <marianabg1@al.insper.edu.br>
2026-04-30 14:49:38 -0300

    ex 5

--- funcoes.py
+++ funcoes.py
@@ -47,4 +47,10 @@ def calcula_pontos_regra_simples(lista_dados):
         pontos[face] = soma
     return pontos
 
+def calcula_pontos_soma(lista):
+    soma = 0 
+    for i in range (len(lista)):
+        soma += i
+    return soma 
+
 
